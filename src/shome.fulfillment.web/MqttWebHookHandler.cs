77f8b1d32b7233a6bd9e60d998d2da2e12503320
Yury Svirko <sv1r4.sd@gmail.com>
2019-08-22 19:59:17 +0300

    fix expect user response playload flag

--- src/shome.fulfillment.web/MqttWebHookHandler.cs
+++ src/shome.fulfillment.web/MqttWebHookHandler.cs
@@ -63,7 +63,7 @@ namespace shome.fulfillment.web
                 {
                     "google", new
                     {
-                        expectUserResponse = isEndInteraction
+                        expectUserResponse = !isEndInteraction
                     }
                 }
             };
