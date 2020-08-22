3cb99c0b35605e317d675109d1b56ab90759628b
Yury Svirko <sv1r4.sd@gmail.com>
2020-08-22 15:30:18 +0300

    remove separate warmup function

--- src/shome.fulfillment.azure.function/FulfillmentFunction.cs
+++ src/shome.fulfillment.azure.function/FulfillmentFunction.cs
@@ -8,22 +8,38 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using shome.fulfillment.azure.function.wakeup;
 
 namespace shome.fulfillment.azure.function
 {
     public class FulfillmentFunction
     {
         private readonly IWebHookHandler _handler;
+        
+        private readonly IWarmupHandler _warmupHandler;
 
-        public FulfillmentFunction(IWebHookHandler handler)
+
+        public FulfillmentFunction(IWebHookHandler handler, IWarmupHandler warmupHandler)
         {
             _handler = handler;
+            _warmupHandler = warmupHandler;
         }
 
         [FunctionName("fulfillment")]
         public async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Function, "post", "get", Route = null)] HttpRequest req,
             ILogger log)
+        {
+            //if get warmup
+            //if post handle dialogflow request
+            if (string.Equals(req.Method, "get", StringComparison.OrdinalIgnoreCase))
+            {
+                return await HandleWarmup(log);
+            }
+            return await HandleHook(req, log);
+        }
+
+        private async Task<IActionResult> HandleHook(HttpRequest req, ILogger log)
         {
             try
             {
@@ -47,6 +63,19 @@ namespace shome.fulfillment.azure.function
             }
         }
 
+        private async Task<IActionResult> HandleWarmup(ILogger log)
+        {
+            try
+            {
+                await _warmupHandler.HandleAsync();
+            }
+            catch (Exception ex)
+            {
+                log.LogError(ex, "Error handle request {function}", nameof(FulfillmentFunction));
+                return new ExceptionResult(ex, true);
+            }
 
+            return new OkResult();
+        }
     }
 }
